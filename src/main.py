30c7bcb6c97887c4127eb51b750ba7c610cd181e
dapoulsen <dapoulsen14@gmail.com>
2026-01-28 21:58:03 +0100

    Added textnode

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-print('hello world')
+from textnode import TextNode, TextType
+
+def main():
+
+    textNode = TextNode("This is some bold text", TextType.BOLD_TEXT)
+    imageNode = TextNode("This is an image", TextType.IMAGE, "www.nicepic.com")
+
+    print(textNode)
+    print(imageNode)
+
+if __name__ == "__main__":
+    main()
+    
