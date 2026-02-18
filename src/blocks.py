b38c481ac11a78ad10ed2581d99aaf8db530c5fa
dapoulsen <dapoulsen14@gmail.com>
2026-02-18 15:35:10 +0100

    started with blocks -> html

--- src/blocks.py
+++ src/blocks.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import HTMLNode
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -41,3 +42,50 @@ def block_to_block_type(block):
             i += 1
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        html_node = block_to_HTMLNode(block, block_type)
+
+
+def block_to_HTMLNode(block, blocktype):
+    match blocktype:
+        case BlockType.HEADING:
+            tag = ""
+            value = ""
+            if block.startswith('# '):
+                tag = "h1"
+                value = block.strip(block[2:])
+            elif block.startswith('## '):
+                tag = "h2"
+                value = block.strip(block[3:])
+            elif block.startswith('### '):
+                tag = "h3"
+                value = block.strip(block[4:])
+            elif block.startswith('#### '):
+                tag = "h4"
+                value = block.strip(block[5:])
+            elif block.startswith('##### '):
+                tag = "h5"
+                value = block.strip(block[6:])
+            elif block.startswith('###### '):
+                tag = "h6"
+                value = block.strip(block[7:])
+            return HTMLNode(tag, value)
+        case BlockType.PARAGRAPH:
+            value = block.replace("\n", " ")
+            return HTMLNode("p", value)
+        case BlockType.CODE:
+            value = block.strip('```')
+            return HTMLNode("code", value)
+        case BlockType.QUOTE:
+            value = block.strip('>')
+            return HTMLNode("blockquote", value)
+        case BlockType.UNORDERED_LIST:
+            value = block.strip('- ')
+            return HTMLNode("ul", value)
+        case BlockType.ORDERED_LIST:
+            value = block.strip('.')
+            return HTMLNode("ol", value)
